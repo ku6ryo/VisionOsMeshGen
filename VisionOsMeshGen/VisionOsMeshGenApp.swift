11c53237cf643869b5dd2f09e15e774952bdfbfb
Ryo Kuroyanagi <im.ryokuro@gmail.com>
2024-02-11 22:37:51 +0900

    Initial Commit

---
+++ VisionOsMeshGen/VisionOsMeshGenApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionOsMeshGenApp.swift
+//  VisionOsMeshGen
+//
+//  Created by Ryo Kuroyanagi on 2024/02/11.
+//
+
+import SwiftUI
+
+@main
+struct VisionOsMeshGenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }.windowStyle(.volumetric)
+    }
+}
